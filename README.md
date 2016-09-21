1d470b3f1147b1f8dcc2d1796010f6f5a7a23d21
Rozsenich Levente <rlevente@INF.ELTE.HU>
2016-09-21 11:07:47 +0200

    2. gyak

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AlkFejl
 A repo for the project
+
+kódok elérhetőek: https://bitbucket.org/gyozke/alkfejl-async/src
---
+++ javaScript.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var g = (a) => { return a*2; }
+
+var h = (a) => a * 2;
+
+var arr = ['alma', 'körte', 'banán', 'barack', 'kiwi'];
+arr.filter(item => item.length === 4);
+
+var nums = [2, 4, 5, 6, 8, 11];
+nums.reduce((x, y) => x + y, 0);
+
+var address = {
+    name: 'Levente',
+    address: 'Budapest',
+    phone: '0630-9400675'
+}
